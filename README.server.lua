673db69f94c9a048045bfb591f8baf93aca1ff0b
NikolaGluh <55589877+NikolaGluh@users.noreply.github.com>
2024-12-24 15:45:57 +0100

    Update and rename README.md to README.server.lua

---
+++ README.server.lua
@@ -0,0 +1,4 @@
+-- To set the plugin up you have to have a folder called BuilderTools with the Main server script inside it
+-- Afterwards just right click the folder and click 'Save as Local Plugin' option
+
+-- P.S. I recommend putting the Main server script inside an actor instance
